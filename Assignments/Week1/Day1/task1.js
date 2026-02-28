0928ed7430ecb7868afddb395b6c16bad051011d
Krishna Rajan Subbiah <krishnarajansubbiah@gmail.com>
2026-02-28 18:58:41 +0100

    Add files via upload

---
+++ Assignments/Week1/Day1/task1.js
@@ -0,0 +1,17 @@
+var firstName = "Subbiah"
+var companyName = `ABC Comp`
+var mobileNumber = 123456
+var isAutomation = true
+var hasPlaywright
+
+//console.log(typeof firstName)
+//console.log(typeof companyName)
+//console.log(typeof mobileNumber)
+//console.log(typeof isAutomation)
+//console.log(typeof hasPlaywright)
+
+console.log("First Name Is: "+firstName)
+console.log(`Company Name is: ${companyName}`)
+console.log("Mobile No Is :"+mobileNumber)
+console.log(`Automation: ${isAutomation}`)
+console.log(hasPlaywright)
